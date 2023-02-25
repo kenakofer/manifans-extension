6ebc442c713af3a53d46459fea61ed64181ac40c
Kenan Schaefkofer <kbitikofer@gmail.com>
2023-02-25 11:25:27 -0800

    Use new API to fetch actual positions
    This is a pretty big commit to move over to use the new API endpoint
    which Ian so kindly set up.
    
    There's some sorting complexity here that can disappear once the API
    endpoint supports ordering and limiting the results returned.
    
    TODO: The extension crashes or stops working and has to be reloaded
    frequently. The user to top positions mapping never refreshes unless it
    gets manually cleared. Same with the permanent market list.  The finger
    icons disappear pretty frequently from the Positions tab.  Fingers
    display on certain small profile images where they shouldn't.

---
+++ manifold-fans-and-critics-extension/constants.js
@@ -0,0 +1,3 @@
+var extension_prefix = 'manifold-fans-and-critics-extension_';
+// const PERM_MARKETS_KEY = extension_prefix + 'perm-markets';
+// const USERNAME_TO_TO_POSITIONS_KEY = extension_prefix + 'user-to-markets';
--- manifold-fans-and-critics-extension/content/content.js
+++ manifold-fans-and-critics-extension/content/content.js
@@ -1,5 +1,45 @@
+const EXTENSION_PREFIX = 'manifold-fans-and-critics-extension_';
+const PERM_MARKETS_KEY = EXTENSION_PREFIX + 'perm-markets';
+const USERNAME_TO_TO_POSITIONS_KEY = EXTENSION_PREFIX + 'user-to-markets';
+
 // Log the current URL
 console.log('Current page URL:', window.location.href);
+
+// const fetchMarketsUrl = 'https://manifold.markets/api/v0/markets?limit=1000';
+const permanentGroupID = '2T4mM0N5az2lYcaN5G50';
+const fetchMarketsUrl = 'https://manifold.markets/api/v0/group/by-id/' + permanentGroupID + '/markets';
+const topSpotCount = 5;
+const necessaryMarketKeys = ['url', 'fanString'];
+
+// The template for fetchPositionsUrl takes a market id
+const fetchPositionsUrl = 'https://manifold.markets/api/v0/market/ID/positions';
+
+// Listen for clicks on the nav with aria-label="Tabs", and replace the images when it is clicked
+document.querySelector('[aria-label="Tabs"]').addEventListener('click', () => {
+  // Wait for the page to load
+  setTimeout(() => {
+    replaceImages();
+  }, 250);
+});
+
+// This is needed to call async functions from the top level of the content script
+(async () => {
+  permMarkets = JSON.parse(localStorage.getItem(PERM_MARKETS_KEY));
+
+  if (!permMarkets) {
+    console.log('Perm markets not found in local storage, fetching from API...');
+    permMarkets = await reloadMarkets();
+  }
+  if (permMarkets && !localStorage.getItem(USERNAME_TO_TO_POSITIONS_KEY)) {
+    await buildUserNameToTopPositions(topSpotCount, permMarkets);
+  }
+
+  replaceImages();
+  console.log('Extension done!');
+  console.log(permMarkets);
+  console.log(JSON.parse(localStorage.getItem(USERNAME_TO_TO_POSITIONS_KEY)));
+})();
+
 
 function replaceImages() {
   console.log("Replacing images...");
@@ -12,11 +52,12 @@ function replaceImages() {
     // Get the alt text on the image, and remove the word avatar from the end
     const imageUsername = image.alt.replace('avatar', '').trim();
 
-    // Load userToMarkets from local storage
-    const userToMarkets = JSON.parse(localStorage.getItem(userToMarketsKey));
+    // Load userNameToTopPositions from local storage
+    const userNameToTopPositions = JSON.parse(localStorage.getItem(USERNAME_TO_TO_POSITIONS_KEY));
+    const permMarkets = JSON.parse(localStorage.getItem(PERM_MARKETS_KEY));
 
-    // Skip if the username is not in userToMarkets
-    if (!userToMarkets[imageUsername]) {
+    // Skip if the username is not in userNameToTopPositions
+    if (!userNameToTopPositions[imageUsername]) {
       return;
     }
 
@@ -62,12 +103,17 @@ function replaceImages() {
       event.stopPropagation();
     });
 
-    userToMarkets[imageUsername].forEach((market) => {
+    userNameToTopPositions[imageUsername].forEach((position) => {
       // Create a new link to this market
       const link = document.createElement('a');
-      link.href = market.url;
+      link.href = permMarkets[position.marketId].url;
       link.target = '_blank';
-      link.textContent = market.fanString + " #1 Fan!";
+      link.textContent = permMarkets[position.marketId].fanString + " #" + position.place +" ";
+      if (position.direction == 'YES') {
+        link.textContent += 'Fan!';
+      } else {
+        link.textContent += 'Critic';
+      }
 
       // Add the link to the hover text
       hoverText.appendChild(link);
@@ -76,91 +122,126 @@ function replaceImages() {
       const newline = document.createElement('br');
       hoverText.appendChild(newline);
     });
-
-    // hoverText.textContent = userToMarkets[imageUsername].map((market) => '<a href="https://google.com">' + market.fanString + " #1 Fan!</a>").join('\n');
-    // hoverText.textContent = userToMarkets[imageUsername].map((market) => 'aoeu' + market.fanString + " #1 Fan!").join('\n');
-
-    // Set the title to the name of all of this user's permanent markets
-    // icon.setAttribute('title', userToMarkets[imageUsername].map((market) => market.fanString + " #1 Fan!").join('\n'));
   });
 }
 
-// const fetchMarketsUrl = 'https://manifold.markets/api/v0/markets?limit=1000';
-const permanentGroupID = '2T4mM0N5az2lYcaN5G50';
-const fetchMarketsUrl = 'https://manifold.markets/api/v0/group/by-id/' + permanentGroupID + '/markets';
-const prefix = 'manifold-fans-and-critics-extension_';
-const permMarketsKey = prefix + 'perm-markets';
-const earliestMarketCheckedKey = prefix + 'earliest-market-id-checked';
-const latestMarketCheckedKey = prefix + 'latest-market-id-checked';
-const userToMarketsKey = prefix + 'user-to-markets';
 
-var permMarkets = JSON.parse(localStorage.getItem(permMarketsKey));
-earliestMarketChecked = localStorage.getItem(earliestMarketCheckedKey);
-latestMarketChecked = localStorage.getItem(latestMarketCheckedKey);
-
-if (!permMarkets) {
-  reloadMarkets();
+function getPositionsUrl(marketId) {
+  return fetchPositionsUrl.replace('ID', marketId);
 }
 
-// Mapping from usernames to a list of permanent market ids they have created
+async function buildUserNameToTopPositions(spots, permMarkets) {
+  console.log('Building username to top positions...');
+  console.log(permMarkets);
+  const userNameToTopPositions = {};
+  const directions = ['YES', 'NO'];
 
-if (permMarkets) {
-  const userToMarkets = {};
-  permMarkets.forEach((market) => {
-    userToMarkets[market.creatorUsername] = userToMarkets[market.creatorUsername] || [];
-    userToMarkets[market.creatorUsername].push(market);
-  });
-  //save userToMarkets to local storage
-  localStorage.setItem(userToMarketsKey, JSON.stringify(userToMarkets));
+  // Loop through each key in the permMarkets object with a standard for loop to accomodate async/await. This is not an array!
+  const market_ids = Object.keys(permMarkets);
+  for (var m = 0; m < market_ids.length; m++) {
+    const market_id = market_ids[m];
+    const market = permMarkets[market_id];
+    console.log("Fetching positions for market " + m + "/" + market_ids.length + ": " + market.fanString);
+
+    var best = {YES: [], NO: []};
+    for (var i = 0; i < spots; i++) {
+      // Initialize position.totalShares.YES and position.totalShares.NO to 0
+      best.YES.push({totalShares: {YES: 0}});
+      best.NO.push({totalShares: {NO: 0}});
+    }
+
+    // Wait for the response before continuing
+    const positionsResponse = await fetch(getPositionsUrl(market_id));
+
+    // Wait for the response to be parsed before continuing
+    const positions = await positionsResponse.json();
+
+    positions.forEach((position) => {
+      directions.forEach((direction) => {
+        // Check if the position has more shares than the lowest position in the best array
+        if (best[direction][spots - 1].totalShares[direction] < position.totalShares[direction]) {
+          insertSorted(best[direction], position, direction);
+        }
+      });
+    });
+
+    // Now we have the top positions for each side of the market
+    // Add these top users to the userNameToTopPositions object
+    directions.forEach((direction) => {
+      // Loop through best[direction], and also keep track of the index with i
+      best[direction].forEach((position, i) => {
+        userNameToTopPositions[position.userUsername] = userNameToTopPositions[position.userUsername] || [];
+        userNameToTopPositions[position.userUsername].push({
+          marketId: market_id,
+          place: i + 1,
+          direction: direction
+        });
+      });
+    });
+  };
+  console.log(userNameToTopPositions);
+
+  //save userNameToTopPositions to local storage
+  localStorage.setItem(USERNAME_TO_TO_POSITIONS_KEY, JSON.stringify(userNameToTopPositions));
 }
 
-replaceImages();
-
-// Listen for clicks on the nav with aria-label="Tabs", and replace the images when it is clicked
-document.querySelector('[aria-label="Tabs"]').addEventListener('click', () => {
-  // Wait for the page to load
-  setTimeout(() => {
-    replaceImages();
-  }, 250);
-});
-
+// Insert a position into the best array, keeping the array sorted from highest to lowest
+function insertSorted(array, position, key) {
+  for (var i = 0; i < array.length; i++) {
+    if (position.totalShares[key] > array[i].totalShares[key]) {
+      array.splice(i, 0, position);
+      array.pop();
+      return;
+    }
+  }
+}
 
 //function to reload all market data
-function reloadMarkets() {
-    // Get the full list of markets, sifting through them for the permanent binary ones
-    permMarkets = [];
+async function reloadMarkets() {
+  console.log('reloadMarkets()');
+  // Get the full list of markets, sifting through them for the permanent binary ones
+  permMarkets = {};
 
-    fetch(fetchMarketsUrl)
-    .then(response => response.json())
-    .then(data => {
-      latestMarketChecked = data[0].id;
-      localStorage.setItem(latestMarketCheckedKey, latestMarketChecked);
+  const marketResponse = await fetch(fetchMarketsUrl);
 
-      data.forEach((market) => {
-          // Only binary markets
-          if (market.outcomeType !== 'BINARY') {
-            return;
-          }
+  // Wait for the response to be parsed before continuing
+  const markets = await marketResponse.json();
 
-          // Only still open markets
-          if (market.isResolved) {
-            return;
-          }
+  markets.forEach((market) => {
+    // Only binary markets
+    if (market.outcomeType !== 'BINARY') {
+      return;
+    }
 
-          // Only markets with permanent in the question
-          // if (!market.question.includes('(Permanent)') && !market.question.includes('[Permanent]')) {
-          //     return;
-          // }
+    // Only still open markets
+    if (market.isResolved) {
+      return;
+    }
 
-          // Set fanstring to be the market question without words in parentheses, and without words like "stock"
-          let ma = market.question.replace(/\(.*?\)/g, '').replace(/\[.*?\]/g, '').replace(/stock/gi, '').trim();
-          market.fanString = ma;
+    // Only markets with permanent in the question
+    // if (!market.question.includes('(Permanent)') && !market.question.includes('[Permanent]')) {
+    //     return;
+    // }
 
-          // Append market to permMarkets
-          permMarkets.push(market);
-      });
-      localStorage.setItem(permMarketsKey, JSON.stringify(permMarkets));
-    })
-    .catch(error => console.error('Error fetching markets:', error));
+    // Set fanstring to be the market question without words in parentheses, and without words like "stock"
+    let ma = market.question.replace(/\(.*?\)/g, '').replace(/\[.*?\]/g, '').replace(/stock/gi, '').trim();
+    market.fanString = ma;
 
+    // Check if each key is in the list of keys to keep (save id to use for the key)
+    var market_id = market.id;
+    for (var key in market) {
+      if (!necessaryMarketKeys.includes(key)) {
+        delete market[key];
+      }
+    }
+
+    // Append market to permMarkets
+    permMarkets[market_id] = market;
+  });
+
+  var storageString = JSON.stringify(permMarkets);
+  console.log('Size of permMarkets going into storage: ' + storageString.length);
+  localStorage.setItem(PERM_MARKETS_KEY, storageString);
+
+  return permMarkets;
 }
